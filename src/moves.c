7b51800eca1c1b336d0cad4d0a53ab5d30512d72
Merel Van der wal <mevan-de@f0r6s14.codam.nl>
2022-05-19 15:08:38 +0200

    move count img protected

--- src/moves.c
+++ src/moves.c
@@ -6,7 +6,7 @@
 /*   By: mevan-de <mevan-de@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/24 16:51:31 by mevan-de      #+#    #+#                 */
-/*   Updated: 2022/05/06 13:55:57 by mevan-de      ########   odam.nl         */
+/*   Updated: 2022/05/19 15:08:12 by mevan-de      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,6 @@ void	count_move(int *move_count, mlx_image_t **img_count, mlx_t *mlx)
 		mlx_delete_image(mlx, *img_count);
 	*img_count = mlx_put_string(mlx, move_count_str, 0, 0);
 	free(move_count_str);
+	if (*img_count == NULL)
+		exit(0);
 }
